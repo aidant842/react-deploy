49afe9da5e923bd53c93ec8ab61d682f45c1cca2
Aidan Thorpe <aidant842@gmail.com>
2021-06-28 10:49:54 +0100

    added api settings

---
+++ src/api.js
@@ -0,0 +1,34 @@
+//BASE URL
+const base_url = "https://api.rawg.io/api/";
+
+//Get the date
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`;
+    } else {
+        return month;
+    }
+};
+
+const getCurrentDay = () => {
+    const day = new Date().getDate();
+    if (day < 10) {
+        return `0${day}`;
+    } else {
+        return day;
+    }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?key=${process.env.REACT_APP_RAWG_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
